97f42fdb83885d759bb51a1863698b4123f56407
Abhijith Ramalingam <ajibith1995@gmail.com>
2016-06-26 22:12:48 -0400

    Added comparisions, using only two features:

--- classifier.py
+++ classifier.py
@@ -1,22 +1,112 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+h = .02  # step size in the mesh
+
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree",
+         "Random Forest", "AdaBoost", "Naive Bayes"
+]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    AdaBoostClassifier(),
+    GaussianNB(),
+]
+figure = plt.figure(figsize=(27, 9))
 
 df = pd.read_csv('breast-cancer-wisconsin.data.csv')
 df.replace('?', -99999, inplace=True)
 df.drop(['id'],1,inplace=True)
+df.drop(['unif_cell_shape','marg_adhesion','single_epith_cell','bare_nuclei','bland_chromatin','normal_nucleoli','mitoses'],1,inplace=True)
 
-x = np.array(df.drop(['class'],1))
+X = np.array(df.drop(['class'],1))
 y = np.array(df['class'])
+X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+# clf = neighbors.KNeighborsClassifier()
+# clf.fit(x_train, y_train)
 
-x_train, x_test, y_train, y_test = cross_validation.train_test_split(x,y,test_size=0.2)
-clf = neighbors.KNeighborsClassifier()
-clf.fit(x_train, y_train)
+# accuracy = clf.score(x_test, y_test)
+# print accuracy
 
-accuracy = clf.score(x_test, y_test)
-print accuracy
+# example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,7,2,1]])
+# example_measures = example_measures.reshape(len(example_measures),-1)
+# prediction = clf.predict(example_measures)
+i = 1
+# X, y = df
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4)
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,7,2,1]])
-example_measures = example_measures.reshape(len(example_measures),-1)
-prediction = clf.predict(example_measures)
-print prediction
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+# just plot the dataset first
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+ax = plt.subplot(1, len(classifiers) + 1, i)
+# Plot the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+i += 1
+
+# iterate over classifiers
+for name, clf in zip(names, classifiers):
+    ax = plt.subplot(1, len(classifiers) + 1, i)
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    # print clf
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    if hasattr(clf, "decision_function"):
+        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        # import pdb; pdb.set_trace()
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+        # continue
+    print xx.shape
+    print yy.size
+    print Z.shape
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+    # Plot also the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+    # and testing points
+    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+               alpha=0.6)
+
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(name)
+    ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+            size=15, horizontalalignment='right')
+    i += 1
+figure.subplots_adjust(left=.02, right=.98)
+plt.show()
