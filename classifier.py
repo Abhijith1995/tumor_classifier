8cbc0148e8b11263652c503a39d2b6552df5f49f
Abhijith Ramalingam <ajibith1995@gmail.com>
2016-06-26 22:36:13 -0400

    Removed stupid commented code

--- classifier.py
+++ classifier.py
@@ -12,47 +12,36 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
-# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 h = .02  # step size in the mesh
 
-names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree",
-         "Random Forest", "AdaBoost", "Naive Bayes"
+names = ["Nearest Neighbors",
+        "Linear SVM",
+        "Decision Tree",
+        "Naive Bayes"
 ]
 
 classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
     DecisionTreeClassifier(max_depth=5),
-    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    AdaBoostClassifier(),
-    GaussianNB(),
+    GaussianNB()
 ]
+columns = ['id','clump_thickness','unif_cell_shape','marg_adhesion','single_epith_cell','bare_nuclei','bland_chromatin','normal_nucleoli','mitoses','class']
 figure = plt.figure(figsize=(27, 9))
 
 df = pd.read_csv('breast-cancer-wisconsin.data.csv')
 df.replace('?', -99999, inplace=True)
-df.drop(['id'],1,inplace=True)
-df.drop(['unif_cell_shape','marg_adhesion','single_epith_cell','bare_nuclei','bland_chromatin','normal_nucleoli','mitoses'],1,inplace=True)
+df.drop('id',1,inplace=True)
+df.drop(columns[1:8],1,inplace=True)
 
 X = np.array(df.drop(['class'],1))
 y = np.array(df['class'])
+
 X = StandardScaler().fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-# clf = neighbors.KNeighborsClassifier()
-# clf.fit(x_train, y_train)
 
-# accuracy = clf.score(x_test, y_test)
-# print accuracy
-
-# example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,7,2,1]])
-# example_measures = example_measures.reshape(len(example_measures),-1)
-# prediction = clf.predict(example_measures)
 i = 1
-# X, y = df
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4)
 
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
 y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
@@ -71,6 +60,8 @@ ax.set_xlim(xx.min(), xx.max())
 ax.set_ylim(yy.min(), yy.max())
 ax.set_xticks(())
 ax.set_yticks(())
+ax.set_title("Full Dataset")
+
 i += 1
 
 # iterate over classifiers
@@ -84,14 +75,11 @@ for name, clf in zip(names, classifiers):
     if hasattr(clf, "decision_function"):
         Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
     else:
-        # import pdb; pdb.set_trace()
         Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-        # continue
-    print xx.shape
-    print yy.size
-    print Z.shape
+
     # Put the result into a color plot
     Z = Z.reshape(xx.shape)
+    print np.unique(Z)
     ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 
     # Plot also the training points
